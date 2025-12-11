c1ded6460942e712a1e80131c504011c63a51117
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-12-11 13:17:12 +0000

    update

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,8 +6,6 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.27.0': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Need by playwright for detecting file system changes during test runs
