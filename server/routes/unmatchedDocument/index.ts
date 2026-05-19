a32148c2cc94f3c2f697532983df38f6195ab7ed
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-05-19 15:33:23 +0100

    CDIA-88: Download unmatched files

--- server/routes/unmatchedDocument/index.ts
+++ server/routes/unmatchedDocument/index.ts
@@ -9,7 +9,7 @@ export default function Index(
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', new UnmatchedDocumentRoutes(documentManagementService).documents,)
+  // get('/', new UnmatchedDocumentRoutes(documentManagementService).documents,)
   get('/:documentId/download', new UnmatchedDocumentRoutes(documentManagementService).download,)
 
   return router
