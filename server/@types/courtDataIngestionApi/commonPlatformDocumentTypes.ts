9ee280bea88c63afff4687cc662eaff450e32a10
ldlharper <ldlharper@gmail.com>
2026-06-02 12:55:49 +0100

    CDIA-108 Fix bug with document type, using API types. (#305)
    Co-authored-by: Luke Harper <luke.harper4@justice.gov.uk>

---
+++ server/@types/courtDataIngestionApi/commonPlatformDocumentTypes.ts
@@ -0,0 +1,20 @@
+const commonPlatformDocumentTypes = [
+  {
+    type: 'SENTENCING_WARRANT',
+    name: 'Sentencing warrant',
+  },
+  {
+    type: 'REMAND_WARRANT',
+    name: 'Remand warrant',
+  },
+  {
+    type: 'COMMON_PLATFORM_DOCUMENT',
+    name: 'Common platform document',
+  },
+  {
+    type: 'PRISON_COURT_REGISTER',
+    name: 'Prison court register',
+  },
+]
+
+export default commonPlatformDocumentTypes
