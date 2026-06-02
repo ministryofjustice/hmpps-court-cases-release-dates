d734cbec5075cdfbab9c776c7d52248d60298d84
ldlharper <ldlharper@gmail.com>
2026-06-02 19:51:22 +0100

    CDIA-108 Fix regression where CP data is missing form CDIA. (#306)
    Co-authored-by: Luke Harper <luke.harper4@justice.gov.uk>

--- server/routes/prisoner/handlers/documents.ts
+++ server/routes/prisoner/handlers/documents.ts
@@ -14,6 +14,7 @@ import { RaSDocumentMapper } from '../../../@types/remandAndSentencingApi/types'
 import CourtDataIngestionService from '../../../services/courtDataIngestionService'
 import { CourtDocument } from '../../../@types/courtDataIngestionApi/types'
 import commonPlatformDocumentTypes from '../../../@types/courtDataIngestionApi/commonPlatformDocumentTypes'
+import expectedTypes from '../../../@types/remandAndSentencingApi/documentTypes'
 
 export default class DocumentRoutes {
   constructor(
@@ -70,8 +71,15 @@ export default class DocumentRoutes {
       if (it.metadata.source === 'court-data-ingestion-api') {
         // From CP
         const cpDocument = cpDocuments.find(itCpDocument => itCpDocument.prisonDocumentId === it.documentUuid)
+        // cpDocument can be missing if data is out of sync between CDIA and document management api.
+        if (cpDocument) {
+          document.typeDescription = commonPlatformDocumentTypes[cpDocument.documentType]?.name
+        } else {
+          document.typeDescription = [...expectedTypes.NON_SENTENCING, ...expectedTypes.SENTENCING].find(
+            type => type.type === it.documentType,
+          ).name
+        }
         document.type = it.documentType
-        document.typeDescription = commonPlatformDocumentTypes.find(type => cpDocument.documentType === type.type).name
         document.isNew = cpDocument ? cpDocument.isUnread : false
       } else {
         // From RaS
