14923bd5a980ba7cbd8a8713483be797ed4e40b6
Tomas Novellino <tomas.novellino1@justice.gov.uk>
2026-09-04 16:19:13 +0100

    CDIA-243: always use court name value from court register. Other minor refactor

--- server/@types/documentManagementApi/types.ts
+++ server/@types/documentManagementApi/types.ts
@@ -63,6 +63,23 @@ export class DocumentManagementMapper {
     return !!(it.metadata?.isUnread)
   }
 
+  public static getCommonPlatformDocumentIds(documents: Document[]): string[] {
+    return documents
+      .filter(it => it.metadata.source === 'court-data-ingestion-api')
+      .map(it => it.documentUuid)
+  }
+
+  public static getCourtCodes(documents: Document[]): string[] {
+    return [
+      ...new Set(
+        documents.filter(it => (it.metadata?.courtCode))
+          .map(it => {
+          return it.metadata.courtCode as string
+        }),
+      ),
+    ]
+  }
+
   static getDownloadHeaders(file: FileDownload) {
     const headers: Map<string, string> = new Map()
 
