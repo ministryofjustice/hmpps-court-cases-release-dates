eea2ed31c610efa788a6476cc556ebb28cc86d17
dmaggio-jd <david.maggio@justice.gov.uk>
2026-09-14 14:04:50 +0100

    RASS-2647 Adding Playwright Integration Tests in place of Cyprus (#392)
    * Adding Playwright Integration Tests in place of Cyprus
    
    * Remove Cypress test files and artefacts
    
    * Adding missing .allowed-scripts.mjs for playwright
    
    * Fixing failing tests
    
    * Fixing failing tests
    
    * Fixing failing tests

--- integration_tests/support/commands.ts
+++
@@ -1,4 +0,0 @@
-Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
-  cy.request('/prisoner/A1234AB/overview')
-  return cy.task('getSignInUrl').then(url => cy.visit(url as string, options))
-})
