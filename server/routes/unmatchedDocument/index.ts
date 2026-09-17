6005f74c8d8b3925ffd93635fddfda50f3353275
Tomas <tomas.novellino1@justice.gov.uk>
2026-09-17 13:25:31 +0100

    CDIA-99: Add role access restriction to unmatched-documents and test coverage (#399)

--- server/routes/unmatchedDocument/index.ts
+++ server/routes/unmatchedDocument/index.ts
@@ -3,12 +3,16 @@ import UnmatchedDocumentRoutes from './UnmatchedDocumentRoutes'
 import DocumentManagementService from '../../services/documentManagementService'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import CourtRegisterService from '../../services/courtRegisterService'
+import requireRole from '../../middleware/requireRole'
+import { Role, Roles } from '../../@types/roles'
 
 export default function Index(
   documentManagementService: DocumentManagementService,
   courtRegisterService: CourtRegisterService,
 ): Router {
   const router = Router()
+  router.use(requireRole(Roles.getRole(Role.COURTCASE_RELEASEDATE_SUPPORT)))
+
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', new UnmatchedDocumentRoutes(documentManagementService, courtRegisterService).documents)
